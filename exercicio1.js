992f3ca93f04bca547be26913896559be95fa425
RODOLFO SILVA FERNANDES DE ARAUJO <rsfaraujo.estagio@mp.mg.gov.br>
2018-09-21 12:37:11 -0300

    'commit'

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,22 @@
-// Faça o exercício dos parágrafos aqui
+﻿// Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+let btoes = document.querySelectorAll(".botao-expandir-retrair");
+
+for (let i = 0; i < btoes.length; i++) {
+   
+	btoes[i].addEventListener('click', function(e) {
+       
+      
+       let p =btoes[i].parentNode
+       console.log(p) 
+        p.classList.toggle('expandido')
+       
+        if (btoes[i].innerHTML==='+'){
+          btoes[i].innerHTML='-'
+        }   
+
+        else {
+           btoes[i].innerHTML='+'
+        }
+  })
+}
--- exercicio2.js
+++ exercicio2.js
@@ -1,4 +1,4 @@
-// Faça o exercício da galeria de imagens aqui
+﻿// Faça o exercício da galeria de imagens aqui
 // Este arquivo ainda não está incluído no arquivo HTML
 
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
@@ -9,3 +9,19 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+  let i = 0;
+  const proximo= document.querySelector("#proximo");
+  const anterior = document.querySelector("#anterior");
+  let slide = document.querySelector("#slide");
+  proximo.addEventListener("click", function(e) {
+    i = (i + 1) % 5;
+    slide.src = servidorDasImagens + todasAsImagens[i];
+  });
+  anterior.addEventListener("click", function(e) {
+    i = i - 1;
+    if (i < 0) {
+      i = 4;
+    }
+    slide.src = servidorDasImagens + todasAsImagens[i];
+  });
